08fce799aa20ae6bbaf4f430249d536026761b85
louishb7 <luishenriquebdf@gmail.com>
2026-09-06 12:10:48 -0300

    ui: add tailwind foundation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,8 +5,7 @@ import App from "./app/App"
 import AppErrorBoundary from "./app/AppErrorBoundary"
 import { AuthProvider } from "./context/AuthContext"
 import { MissionBoardProvider } from "./context/MissionBoardContext"
-import "./theme/tokens.css"
-import "./styles.css"
+import "./theme/v2.css"
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
